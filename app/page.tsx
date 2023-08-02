65f967d9df090023ef0c8b9359f00559d737e960
Raagul N <raagul72@gmail.com>
2023-08-02 20:00:40 +0530

    updated homepage text

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,16 @@ export default function Home() {
       <Title hasHomeRoute={false}>raagul nagendran</Title>
       <p>
         Your friendly neighbourhood{" "}
-        <span className={spanCN}>Data Scientist / Software Engineer</span>{" "}
-        currently based out of Seattle. I love digging into data to uncover
-        trends, build highly custom and accessible software experiences and
-        optimizing for the fastest user experience.
+        <span className={spanCN}>Data Scientist / Software Engineer</span>.
+      </p>
+      <p>
+        You can find me exploring real-life applications of{" "}
+        <span className={spanCN}>Natural Language Processing</span> and{" "}
+        <span className={spanCN}>Computer Vision</span> as a part of my Data
+        Science graduate degree in University of Washington, Seattle. I love
+        developing digging into data to uncover trends, build highly custom and
+        accessible software experiences and optimizing for the fastest user
+        experience.
       </p>
       <p>
         To check out my craft at previous companies, you can visit the{" "}
@@ -33,10 +39,10 @@ export default function Home() {
         about them.
       </p>
 
-      <p>
+      {/* <p>
         Got no time to go through all my work? No problem, here's an elevator
         pitch.
-      </p>
+      </p> */}
       <p>
         You can find me on{" "}
         <PrettyLink href="https://github.com/beyondtheinferno" target="_blank">
